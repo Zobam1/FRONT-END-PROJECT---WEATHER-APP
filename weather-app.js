1453b772b9caff33c55ccb613e4fbbe650295eec
Zobam1 <nnojemabel@gmail.com>
2023-12-22 17:40:32 +0100

    added required data from the weather api

--- weather-app.js
+++ weather-app.js
@@ -1,27 +1,34 @@
-let grabDate = document.querySelector("#date");
-let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let hour = now.getHours();
-let minute = now.getMinutes();
-grabDate.innerHTML = `${day}, ${hour}:${minute}`;
-
 function showValues(response) {
-  let city2 = document.querySelector("#city-input");
-  city2 = city2.value;
   let h1 = document.querySelector("h1");
-  h1.innerHTML = city2;
-  let temp = response.data.temperature.current;
+  h1.innerHTML = response.data.city;
+  let temp = Math.round(response.data.temperature.current);
   let temps = document.querySelector("#temps");
   temps.innerHTML = temp;
+  let status = document.querySelector("#condition");
+  status.innerHTML = response.data.condition.description;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = `${response.data.wind.speed}km/h`;
+  let iconUrl = response.data.condition.icon_url;
+  let icon = document.querySelector("#weather-icon");
+  icon.innerHTML = `<img src=${iconUrl} alt="weather-icon">`;
+  console.log(response);
+  let grabDate = document.querySelector("#date");
+  let urlDate = new Date(response.data.time * 1000);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[urlDate.getDay()];
+  let hour = urlDate.getHours();
+  let minute = urlDate.getMinutes();
+  grabDate.innerHTML = `${day}, ${hour}:${minute}`;
 }
 
 function callAxios(event) {
@@ -35,3 +42,9 @@ function callAxios(event) {
 
 let grabForm = document.querySelector("#form");
 grabForm.addEventListener("submit", callAxios);
+
+axios
+  .get(
+    "https://api.shecodes.io/weather/v1/current?query=paris&key=at32a2043d2f18b00363437fb0ffa1ob&units=metric"
+  )
+  .then(showValues);
